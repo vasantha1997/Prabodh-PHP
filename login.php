5d20671f4fdec51288ddcb6478694d66add929a0
vasantha1997 <vasantha@prabodh.in>
2023-03-01 22:02:50 +0530

    developed php application

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+// $url = "welcome.html";
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if($_POST)
+{
+$host="localhost";
+$dbusername="root";
+$dbpassword="";
+$dbname="prabodh";
+$firstname = $_POST['username'];
+$password = $_POST['password'];
+$conn=mysqli_connect($host,$dbusername,$dbpassword,$dbname);
+$query="SELECT * from prabodh where username='$username' and password='$password'";
+	  $result=mysqli_query($conn,$query);
+	  if(mysqli_num_rows($result)==1)
+	  {
+		 header("location:welcome.html?username=$username");
+        // header("location:welcome.php?username=$username");
+         echo "<div>".$username." is login.</div>";        
+
+	  }
+	  else {
+	     echo 'Wrong USERNAME or PASSWORD';
+		 }
+
+}		 
+?>
